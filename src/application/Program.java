2c55869db3b22fddb2c8e1bce456da89197a6056
Ana julia <anajuliapereiradesouza003@gmail.com>
2024-07-18 09:39:52 -0300

    Handling exceptions and clearing screen

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,10 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import boardgame.Board;
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
@@ -10,32 +12,51 @@ import chess.ChessPosition;
 public class Program {
 
 	public static void main(String[] args) {
-		
+		/*
 		System.out.println();
 		Description d = new Description();
 		System.out.println(d.toString());
-		System.out.println();
+		System.out.println();*/
 
 		Scanner sc = new Scanner(System.in);
 		
 		ChessMatch chessMatch = new ChessMatch();
 		
 		while(true) {
-
-			UI.printBoard(chessMatch.getPieces());
-			System.out.println();
-			System.out.print("---------------\n");
-			System.out.print("Posição Origem: ");
-			ChessPosition source = UI.readChessPosition(sc);
-			
-			System.out.println();
-			System.out.print("Posição Destino: ");
-			ChessPosition target = UI.readChessPosition(sc);
-			System.out.print("---------------\n");
-			
-			System.out.println();
-			
-			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			try {
+				
+				UI.clearScreen();
+				
+				System.out.println();
+				Description d = new Description();
+				System.out.println(d.toString());
+				System.out.println();
+				
+				UI.printBoard(chessMatch.getPieces());
+				System.out.println();
+				System.out.print("---------------\n");
+				System.out.print("Posição Origem: ");
+				ChessPosition source = UI.readChessPosition(sc);
+				
+				System.out.println();
+				System.out.print("Posição Destino: ");
+				ChessPosition target = UI.readChessPosition(sc);
+				System.out.print("---------------\n");
+				
+				System.out.println();
+				
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			}
+			catch (ChessException e) {
+				System.out.println(e.getMessage());
+				System.out.print("Pressione [ENTER] para continuar");
+				sc.nextLine();
+			}
+			catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				System.out.print("Pressione [ENTER] para continuar");
+				sc.nextLine();
+			}
 		}
 		
 		
--- src/application/UI.java
+++ src/application/UI.java
@@ -30,6 +30,11 @@ public class UI {
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 	
+	public static void clearScreen() {
+		 System.out.print("\033[H\033[2J");
+		 System.out.flush();
+	} 
+	
 	
 	public static ChessPosition readChessPosition(Scanner sc) {
 		try {
@@ -40,7 +45,7 @@ public class UI {
 		}
 		catch (RuntimeException e) {
 			// TODO: handle exception
-			throw new InputMismatchException("Erro lendo posição de xadrez. Valores valido sao de a1 até h8");
+			throw new InputMismatchException("Erro lendo posição de xadrez. Valores valido sao de [a1] até [h8]");
 		}
 		
 	}
