d6ba43b64023c881cb2cfd8dd69a48dbe786daaf
Ana julia <anajuliapereiradesouza003@gmail.com>
2024-07-21 22:38:57 -0300

    Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -58,6 +58,13 @@ public class Program {
 				if(capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
+				
+				if (chessMatch.getPromoted() != null) {
+					System.out.print("Entre com a peca para promover! (B/N/R/Q): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
+				
 			}
 			catch (ChessException e) {
 				System.out.println(e.getMessage());
